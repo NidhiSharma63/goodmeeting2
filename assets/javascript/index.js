1a8e2a681c7f60be025e5f21e493927caa3c62ff
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-03-25 17:37:53 +0530

    testimonial slider bug fixed

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,42 +1,58 @@
 $(document).ready(() => {
-
+    let timer = true;
     const testimonialTextContainer = document.querySelector(".testimonialTextContainer")
     let width = testimonialTextContainer.offsetWidth;
     const testimonialSlider = document.querySelector(".testimonialSlider");
-    const testimonials = document.querySelectorAll(".testimonial");
+    const testimonialContent = document.querySelectorAll(".testimonialContent");
     const testimonialDot = document.querySelectorAll(".testimonialDot");
     window.addEventListener('resize', function () {
         width = testimonialTextContainer.offsetWidth;
     });
+    console.log(testimonialContent.length)
     let count = 0;
     const nexSlide = () => {
-        if (count < testimonials.length - 1) {
+        if (count < testimonialContent.length) {
             testimonialSlider.style.left = -width * count + "px"
             testimonialDot[count].classList.add("opacity");
         }
-        count++
-        if (count == testimonials.length) {
+        testimonialDot.forEach((dot) => {
+            dot.addEventListener("click", (e) => {
+                timer = false;
+                console.log("count" + count)
+                if (count != undefined) {
+                    if (count != e.target.dataset.testimonial) {
+                        testimonialDot[count].classList.remove("opacity");
+                    }
+                }
+                testimonialDot[e.target.dataset.testimonial].classList.add("opacity");
+                count = e.target.dataset.testimonial;
+                testimonialSlider.style.left = -width * count + "px"
+                setTimeout(() => {
+                    timer = true
+                }, 8000);
+            })
+        })
+
+        if (count == testimonialContent.length) {
             count = 0;
             testimonialSlider.style.left = 0 + "px"
             testimonialDot[count].classList.add("opacity");
         }
     }
-    setInterval(() => {
-        // remove class opacity from last dot
-        if (count > 0 && count < testimonials.length) {
+    const mainFunction = () => {
+        // remove class opacity from last dot and call nextslide function
+        if (count > 0 && count <= testimonialContent.length) {
             testimonialDot[count - 1].classList.remove("opacity");
 
         }
         nexSlide();
+    }
+    setInterval(() => {
+        if (timer) {
+            count++
+            mainFunction();
+        }
     }, 3000);
 
-    testimonialDot.forEach((dot) => {
-        dot.addEventListener("click", () => {
-            testimonialSlider.style.left = -width * dot.dataset.testimonial + "px";
-            testimonialDot[count].classList.remove("opacity");
-            testimonialDot[dot.dataset.testimonial].classList.add("opacity");
-        })
-    })
-
-    window.onload = nexSlide();
+    mainFunction();
 });
