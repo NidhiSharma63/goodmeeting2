88da314f156193c18b45b4d3febdacaf8d45576f
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-03-26 17:51:55 +0530

    touch touch move bug fixed

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -42,13 +42,14 @@ const nexSlide = () => {
         // if slide is move then make current testimonialSlide = testimonialSlide + 1
         e.preventDefault();
         if (touchStart > touchend) {
-            if (testimonialSlide < testimonialSlider.children.length - 1 && testimonialSlide > -1) {
+            if (testimonialSlide < testimonialSlider.children.length - 1 && testimonialSlide >= -1) {
                 testimonialSlider.style.left = -testimonialTextContainerWidth * (testimonialSlide + 1) + "px";
                 testimonialDot[testimonialSlide + 1].classList.add("opacity");
                 testimonialDot[testimonialSlide].classList.remove("opacity");
                 setTimeout(() => {
-                    moveSlide = true
+                    moveSlide = true;
                 }, 8000);
+                testimonialSlide = testimonialSlide + 1
             }
         }
         if(touchend>touchStart){
@@ -57,15 +58,7 @@ const nexSlide = () => {
                 setTimeout(() => {
                     moveSlide = true
                 }, 8000);
-            }
-        }
-
-        if (isTestimonialTouchMove) {
-            if (touchend > touchStart) {
                 testimonialSlide = testimonialSlide - 1;
-            }
-            if (touchend < touchStart) {
-                testimonialSlide = testimonialSlide + 1
             }
         }
     })
