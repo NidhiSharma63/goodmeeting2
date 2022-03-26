f281111bc17f9da77307b96b30947d982f85811a
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-03-26 16:36:42 +0530

    touch slider added

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,4 +1,3 @@
-
 const testimonialTextContainer = document.querySelector(".testimonialTextContainer")
 const dashboardImageContainer = document.querySelector(".dashboardImageContainer");
 let testimonialTextContainerWidth = testimonialTextContainer.offsetWidth;
@@ -13,23 +12,73 @@ window.addEventListener('resize', function () {
 });
 // initialize varibale
 let testimonialSlide = 0;
-let dashboardSlide=0;
+let dashboardSlide = 0;
 let moveSlide = true;
-
+let isTestimonialTouchMove;
+let touchStart;
+let  touchend;
 const nexSlide = () => {
     // if testimonialSlide less than testimonialSlider.children.length and dashboardImagesSlider.children.length
     if (testimonialSlide < (testimonialSlider.children.length)) {
         testimonialSlider.style.left = -testimonialTextContainerWidth * testimonialSlide + "px";
         testimonialDot[testimonialSlide].classList.add("opacity");
     }
+    // make slide move on Touch and do isTestimonialTouchMove = false
+    testimonialSlider.addEventListener("touchstart", (e) => {
+        e.preventDefault();
+        console.log("touch start  "+e.targetTouches[0].clientX);
+        touchStart = e.targetTouches[0].clientX;
+        isTestimonialTouchMove = false;
+    });
+    testimonialSlider.addEventListener("touchmove", (e) => {
+        // when touch is move then do move slide false and isTestimonialTouchMove = true;
+        console.log("touch move"+e.targetTouches[0].clientX);
+         touchend = e.targetTouches[0].clientX;
+
+        moveSlide = false;
+        isTestimonialTouchMove = true;
+        e.preventDefault();
+        if(touchStart > touchend){
+            if (testimonialSlide < testimonialSlider.children.length - 1) {
+                testimonialSlider.style.left = -testimonialTextContainerWidth * (testimonialSlide + 1) + "px";
+                testimonialDot[testimonialSlide + 1].classList.add("opacity");
+                testimonialDot[testimonialSlide].classList.remove("opacity");
+                setTimeout(() => {
+                    moveSlide = true
+                }, 8000);
+            }
+        }
+        if(touchend > touchStart){
+            if (testimonialSlide > 0) {
+                testimonialSlider.style.left = -testimonialTextContainerWidth * (testimonialSlide - 1) + "px";
+                testimonialDot[testimonialSlide - 1].classList.add("opacity");
+                testimonialDot[testimonialSlide].classList.remove("opacity");
+                setTimeout(() => {
+                    moveSlide = true
+                }, 8000);
+            }
+        }
+    });
+    testimonialSlider.addEventListener("touchend", (e) => {
+        // if slide is move then make current testimonialSlide = testimonialSlide + 1
+        e.preventDefault();
+        if (isTestimonialTouchMove) {
+            if(touchend > touchStart){
+                 testimonialSlide = testimonialSlide - 1
+            }
+            if(touchend < touchStart){
+                testimonialSlide = testimonialSlide + 1
+            }
+        }
+    })
     // addEventListener on each dot
     testimonialDot.forEach((dot) => {
         dot.addEventListener("click", (e) => {
             // make moveSlide = false
             moveSlide = false;
-                if (testimonialSlide != e.target.dataset.testimonial) {
-                    testimonialDot[testimonialSlide].classList.remove("opacity");
-                }
+            if (testimonialSlide != e.target.dataset.testimonial) {
+                testimonialDot[testimonialSlide].classList.remove("opacity");
+            }
             testimonialDot[e.target.dataset.testimonial].classList.add("opacity");
             testimonialSlide = e.target.dataset.testimonial;
             testimonialSlider.style.left = -testimonialTextContainerWidth * testimonialSlide + "px"
@@ -48,7 +97,7 @@ const nexSlide = () => {
 const mainTestimonialFunction = () => {
     // remove class opacity from last dot and call nextslide function
     if (testimonialSlide > 0 && testimonialSlide <= testimonialSlider.children.length) {
-       testimonialDot[testimonialSlide - 1].classList.remove("opacity");
+        testimonialDot[testimonialSlide - 1].classList.remove("opacity");
 
     }
     nexSlide();
@@ -66,9 +115,9 @@ mainTestimonialFunction();
 
 const nexDashboardSlide = () => {
     // if count less  dashboardImagesSlider.children.length 
-    if ( dashboardSlide < (dashboardImagesSlider.children.length)) {
+    if (dashboardSlide < (dashboardImagesSlider.children.length)) {
         // adding 2 to dashboardImagesSlider left to make fit the image
-        dashboardImagesSlider.style.left = - dashboardImageContainerWidth * dashboardSlide +2+ "px";
+        dashboardImagesSlider.style.left = -dashboardImageContainerWidth * dashboardSlide + 2 + "px";
         dashboardDots[dashboardSlide].classList.add("opacity");
     }
     // addEventListener on each dot
@@ -76,12 +125,12 @@ const nexDashboardSlide = () => {
         dot.addEventListener("click", (e) => {
             // make moveSlide = false
             moveSlide = false;
-                if (dashboardSlide != e.target.dataset.dashboard) {
-                    dashboardDots[dashboardSlide].classList.remove("opacity");
-                }
+            if (dashboardSlide != e.target.dataset.dashboard) {
+                dashboardDots[dashboardSlide].classList.remove("opacity");
+            }
             dashboardDots[e.target.dataset.dashboard].classList.add("opacity");
             dashboardSlide = e.target.dataset.dashboard;
-            dashboardImagesSlider.style.left = -dashboardImageContainerWidth * dashboardSlide +2+ "px"
+            dashboardImagesSlider.style.left = -dashboardImageContainerWidth * dashboardSlide + 2 + "px"
             setTimeout(() => {
                 moveSlide = true
             }, 8000);
@@ -97,7 +146,7 @@ const nexDashboardSlide = () => {
 const mainDashboardFunction = () => {
     // remove class opacity from last dot and call nextslide function
     if (dashboardSlide > 0 && dashboardSlide <= dashboardImagesSlider.children.length) {
-       dashboardDots[dashboardSlide - 1].classList.remove("opacity");
+        dashboardDots[dashboardSlide - 1].classList.remove("opacity");
 
     }
     nexDashboardSlide();
