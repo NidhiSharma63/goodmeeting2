e75d8ba8c676cfac23f2cbccbcfb284ee97660fb
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-04-02 17:18:04 +0530

    fix:conflict

--- assets/javascript/oops.js
+++ assets/javascript/oops.js
@@ -6,7 +6,6 @@ const testimonialSlider = document.querySelector(".testimonialSlider");
 const testimonialDot = document.querySelectorAll(".testimonialDot");
 const dashboardImagesSlider = document.querySelector(".dashboardImagesSlider");
 const dashboardDots = document.querySelectorAll(".dashboardDot");
-const testimonialContent = document.querySelectorAll(".testimonialContent");
 const dashboardImg = document.querySelectorAll(".dashboardImg");
 // initialize varibale for testimonialand dashboard slide
 let currentTestimonialSlide = 0;
@@ -15,10 +14,8 @@ let testimonialMoveSlide = true;
 let testimonialTouchStart, dashboardTouchStart;
 let testimonialTouchEnd, dashboardTouchEnd;
 let dashboardMoveSlide = true;
-
-//geting the values
-const totalTestinomialSlideLength = testimonialSlider.children.length;
 const totalDashboardSlideLength = dashboardImagesSlider.children.length;
+
 // move slides
 class CommonVaribles {
     constructor({
@@ -27,7 +24,7 @@ class CommonVaribles {
         slider,
         containerWidth,
         dots
-    }={}) {
+    } = {}) {
         this.currentSlide = currentSlide;
         this.totalSlideLength = totalSlideLength;
         this.slider = slider;
@@ -35,6 +32,11 @@ class CommonVaribles {
         this.dots = dots;
     }
 }
+const fetchData = async () => {
+    const response = await fetch('./assets/javascript/testinomial.json');
+    return response.json();
+}
+
 
 class MoveSlides extends CommonVaribles {
     showNextSlide() {
@@ -99,10 +101,10 @@ class MoveSlides extends CommonVaribles {
     }
     // get the touchstart value
     getTouchStart(e) {
-        if(e.target.classList.contains("testimonialText")){
+        if (e.target.classList.contains("testimonialText")) {
             testimonialTouchStart = e.touches[0].clientX;
         }
-        if(e.target.classList.contains("dashboardImg")){
+        if (e.target.classList.contains("dashboardImg")) {
             dashboardTouchStart = e.touches[0].clientX;
         }
     }
@@ -148,41 +150,66 @@ class MoveSlides extends CommonVaribles {
         }
     }
 }
+const showData = async () => {
+    const data = await fetchData();
+    data.forEach((item) => {
+        testimonialSlider.innerHTML +=
+            `<div class="testimonialContent">
+        <div class="testimonialText letterSpacing font1 lightBlackColor">${item.review}</div>
+        <div class="testimonialUser">
+        <p class="userName font1 secondaryColor">${item.reviewer}</p>
+        <p class="userStatus font1 secondaryColor">${item.status}</p>
+        </div>
+        </div>`
+    })
+    // getting values
+    const testimonialContent = document.querySelectorAll(".testimonialContent");
+    const totalTestinomialSlideLength = testimonialSlider.children.length;
+    // declaring the class
+    const moveTestimonialSlide = new MoveSlides({
+        currentSlide: currentTestimonialSlide,
+        totalSlideLength: totalTestinomialSlideLength,
+        slider: testimonialSlider,
+        containerWidth: testimonialTextContainerWidth,
+        dots: testimonialDot
+    });
 
-const moveTestimonialSlide = new MoveSlides({
-    currentSlide: currentTestimonialSlide,
-    totalSlideLength: totalTestinomialSlideLength,
-    slider: testimonialSlider,
-    containerWidth: testimonialTextContainerWidth,
-    dots: testimonialDot
-});
+    testimonialContent.forEach(content => {
+        content.addEventListener("touchstart", (e) => {
+            e.preventDefault();
+            moveTestimonialSlide.getTouchStart(e);
+        });
+        content.addEventListener("touchmove", (e) => {
+            e.preventDefault();
+            moveTestimonialSlide.getTouchMove(e);
+        });
+        content.addEventListener("touchend", (e) => {
+            e.preventDefault();
+            if (testimonialTouchStart > testimonialTouchEnd) {
+                moveTestimonialSlide.touchEndNextSlideHandler(e);
+            }
+            if (testimonialTouchStart < testimonialTouchEnd) {
+                moveTestimonialSlide.touchEndPreviousSlideHandler(e);
+            }
+        });
+    });
+    setInterval(() => {
+        // first call increaseSlide:to increase slide
+        // second call showNextSlide:to show next slide
+        if (testimonialMoveSlide) {
+            moveTestimonialSlide.increaseSlide();
+            moveTestimonialSlide.showNextSlide();
+        }
+    }, 3000);
+    moveTestimonialSlide.showNextSlide();
+    moveTestimonialSlide.dotsClickHandler();
+}
 const moveDashboardSlide = new MoveSlides({
     currentSlide: currentDashboardSlide,
     totalSlideLength: totalDashboardSlideLength,
     slider: dashboardImagesSlider,
     containerWidth: dashboardImageContainerWidth,
     dots: dashboardDots
-});
-
-// touch start and end
-testimonialContent.forEach(content => {
-    content.addEventListener("touchstart", (e) => {
-        e.preventDefault();
-        moveTestimonialSlide.getTouchStart(e);
-    });
-    content.addEventListener("touchmove", (e) => {
-        e.preventDefault();
-        moveTestimonialSlide.getTouchMove(e);
-    });
-    content.addEventListener("touchend", (e) => {
-        e.preventDefault();
-        if(testimonialTouchStart > testimonialTouchEnd){
-            moveTestimonialSlide.touchEndNextSlideHandler(e);
-        }
-        if(testimonialTouchStart < testimonialTouchEnd){
-            moveTestimonialSlide.touchEndPreviousSlideHandler(e);
-        }
-    });
 });
 dashboardImg.forEach(img => {
     img.addEventListener("touchstart", (e) => {
@@ -195,10 +222,10 @@ dashboardImg.forEach(img => {
     });
     img.addEventListener("touchend", (e) => {
         e.preventDefault();
-        if(dashboardTouchStart > dashboardTouchEnd){
+        if (dashboardTouchStart > dashboardTouchEnd) {
             moveDashboardSlide.touchEndNextSlideHandler(e);
         }
-        if(dashboardTouchStart < dashboardTouchEnd){
+        if (dashboardTouchStart < dashboardTouchEnd) {
             moveDashboardSlide.touchEndPreviousSlideHandler(e);
         }
     });
@@ -206,18 +233,13 @@ dashboardImg.forEach(img => {
 setInterval(() => {
     // first call increaseSlide:to increase slide
     // second call showNextSlide:to show next slide
-    if (testimonialMoveSlide) {
-        moveTestimonialSlide.increaseSlide();
-        moveTestimonialSlide.showNextSlide();
-    }
     if (dashboardMoveSlide) {
         moveDashboardSlide.increaseSlide();
         moveDashboardSlide.showNextSlide();
     }
 }, 3000);
 window.onload = () => {
-    moveTestimonialSlide.showNextSlide();
+    showData();
     moveDashboardSlide.showNextSlide();
-    moveTestimonialSlide.dotsClickHandler();
     moveDashboardSlide.dotsClickHandler();
 }
