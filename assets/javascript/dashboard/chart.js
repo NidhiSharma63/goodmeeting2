2acc2b2fc13d5a2aa8b36967c55f4071a95aa99a
NidhiSharma63 <nidhisharma63593@gmail.com>
2022-04-09 13:09:26 +0530

    added:gradientColor added

--- assets/javascript/dashboard/chart.js
+++ assets/javascript/dashboard/chart.js
@@ -3,7 +3,15 @@ const Score = [10, 40, 50, 80, 60, 100]
 const ctx1 = document.getElementById('canva1').getContext('2d');
 const ctx2 = document.getElementById('canva2').getContext('2d');
 const ctx3 = document.getElementById('canva3').getContext('2d');
-const avgAnswerFeedback = [2,6,8,1,4,10] 
+var gradientBg = ctx2.createLinearGradient(0, 0, 0, 400);
+gradientBg.addColorStop(0, 'rgba(229,80,80,1)');  
+gradientBg.addColorStop(0.3, 'rgba(229,80,80,0.99)');
+gradientBg.addColorStop(1, 'rgba(22,80,80,0.29)');
+var gradientBg2 = ctx3.createLinearGradient(0, 0, 0, 400);
+gradientBg2.addColorStop(0, 'rgb(41,99,224)');  
+gradientBg2.addColorStop(0.4, 'rgb(41,99,224,0.79)');
+gradientBg2.addColorStop(1, 'rgb(41,99,224,0.29)');
+const avgAnswerFeedback = [2, 6, 8, 1, 4, 10]
 const myChart = new Chart(ctx1, {
     type: 'bar',
     data: {
@@ -59,16 +67,22 @@ const myChart = new Chart(ctx1, {
         }
     }
 });
+
 const myChart2 = new Chart(ctx2, {
     type: 'line',
     data: {
         labels: Meetings,
         datasets: [{
+            fill: true,
+            pointColor: "#fff",
+            pointBackgroundColor: '#fff',
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "#fff",
             label: 'Meetings Score',
             data: avgAnswerFeedback,
-            backgroundColor: 'rgb(229,80,80)',
-            borderColor:'rgb(229,80,80)',
-            tension:.3,
+            backgroundColor: gradientBg,
+            borderColor: 'rgb(229,80,80)',
+            tension: .3,
         }]
     },
     options: {
@@ -102,7 +116,7 @@ const myChart2 = new Chart(ctx2, {
                         size: 10
                     },
                     boxWidth: 20,
-                    boxHeight:15,
+                    boxHeight: 15,
                 }
             },
         }
@@ -113,11 +127,16 @@ const myChart3 = new Chart(ctx3, {
     data: {
         labels: Meetings,
         datasets: [{
+            fill: true,
+            pointColor: "#fff",
+            pointBackgroundColor: '#fff',
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "#fff",
             label: 'Meetings Score',
             data: avgAnswerFeedback,
-            backgroundColor: 'rgb(41,99,224)',
-            borderColor:'rgb(41,99,224)',
-            tension:.3,
+            backgroundColor:gradientBg2 ,
+            borderColor: 'rgb(41,99,224)',
+            tension: .3,
         }]
     },
     options: {
@@ -149,12 +168,12 @@ const myChart3 = new Chart(ctx3, {
                 labels: {
                     // padding:43,
                     color: 'rgba(225,225,225)',
-                    textAlign:'right',
+                    textAlign: 'right',
                     font: {
                         size: 10
                     },
                     boxWidth: 20,
-                    boxHeight:15,
+                    boxHeight: 15,
                 }
             },
         }
