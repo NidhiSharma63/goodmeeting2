48b70d21321d128b5f7eab1a6dcc5984d6002fa8
NidhiSharma63 <nidhisharma63593@gmail.com>
2022-04-12 10:05:49 +0530

    refractor:navbar fixed

--- assets/javascript/dashboard/main.js
+++ assets/javascript/dashboard/main.js
@@ -1,15 +1,4 @@
 $(document).ready(function () {
-    // $('.moon').click(() => {
-    //     const getModeValue = localStorage.getItem("goodmeeting_today_color_scheme");
-    //         if(getModeValue==='dark'){
-    //             $(document.body).removeClass("darkTheme");
-    //             localStorage.setItem("goodmeeting_today_color_scheme", 'light');
-    //         }else{
-    //             $(document.body).addClass("darkTheme");
-    //             localStorage.setItem("goodmeeting_today_color_scheme", 'dark');
-    //         }
-    // })
-
     const handleSideBar = () => {
         $('.contentContainer').toggleClass('shrinkContainerWidht');
         $('.iconsInfo').toggleClass('show');
