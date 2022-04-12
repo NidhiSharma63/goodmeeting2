4e265ca5d7e5f1e66ec0aa79f6237cb3309126d5
NidhiSharma63 <nidhisharma63593@gmail.com>
2022-04-12 12:49:36 +0530

    fix:sidebar on small width

--- assets/javascript/dashboard/main.js
+++ assets/javascript/dashboard/main.js
@@ -4,23 +4,57 @@ $(document).ready(function () {
         $('.iconsInfo').toggleClass('show');
         $('.iconsContainer').toggleClass('boxShadowRemove');
     }
-    $('.sideBar').click(() => {
-        handleSideBar();
-    });
-    $('.stat1').click(() => {
-        handleSideBar();
-    });
-    $('.stat2').click(() => {
-        handleSideBar();
-    });
-    $('.stat3').click(() => {
-        handleSideBar();
-    });
-    $('.help').click(() => {
-        handleSideBar();
-    });
-    $('.feedback').click(() => {
-        handleSideBar();
-    });
+    const handleSideBarOnWidthChange = () => {
+        $('.iconsInfo').toggleClass('show');
+        $('.iconsContainer').toggleClass('boxShadowRemove');
+    }
    
+    if (matchMedia) {
+        var mq2 = window.matchMedia("(max-width: 965px)");
+        mq2.addListener(WidthChange2);
+        WidthChange2(mq2);
+    }
+    // media query change
+    function WidthChange2(width2) {
+        if (width2.matches) {
+            $('.sideBar').click(() => {
+                handleSideBarOnWidthChange();
+            });
+            $('.stat1').click(() => {
+                handleSideBarOnWidthChange();
+            });
+            $('.stat2').click(() => {
+                handleSideBarOnWidthChange();
+            });
+            $('.stat3').click(() => {
+                handleSideBarOnWidthChange();
+            });
+            $('.help').click(() => {
+                handleSideBarOnWidthChange();
+            });
+            $('.feedback').click(() => {
+                handleSideBarOnWidthChange();
+            });
+        } else {
+            $('.sideBar').click(() => {
+                handleSideBar();
+            });
+            $('.stat1').click(() => {
+                handleSideBar();
+            });
+            $('.stat2').click(() => {
+                handleSideBar();
+            });
+            $('.stat3').click(() => {
+                handleSideBar();
+            });
+            $('.help').click(() => {
+                handleSideBar();
+            });
+            $('.feedback').click(() => {
+                handleSideBar();
+            });
+        }
+    }
+  
 });
